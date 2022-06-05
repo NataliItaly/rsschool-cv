2b82fc2b5b09f86b709c9fd2ac7445e6e828f9f9
Krylova Nataliya <natalinebo2014@yandex.ru>
2022-06-05 18:36:45 +0200

    feat: add project items

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
-const BURGER = document.querySelector('.burger');
-const HEADERMENU = document.querySelector('.header-menu');
+const BURGER = document.querySelector(".burger");
+const HEADERMENU = document.querySelector(".header-menu");
 
-BURGER.addEventListener('click', function () {
-    BURGER.classList.toggle('burger-active');
-    HEADERMENU.classList.toggle('menu-open');
+BURGER.addEventListener("click", function () {
+  BURGER.classList.toggle("burger-active");
+  HEADERMENU.classList.toggle("menu-open");
 });
-HEADERMENU.addEventListener('click', function () {
-    BURGER.classList.toggle('burger-active');
-    HEADERMENU.classList.toggle('menu-open');
+HEADERMENU.addEventListener("click", function () {
+  BURGER.classList.toggle("burger-active");
+  HEADERMENU.classList.toggle("menu-open");
 });
 
+let a = "jgjhgj";
+
+for (let i = 0; i < a.length; i++) {}
